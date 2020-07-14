87c7a4a64a1c24c24f8d6b8f7f88b55e26f8f035
Margret `Pax` Williams <pax.margret@gmail.com>
2020-07-14 16:13:43 -0700

    :sparkles: Get person with highest net income

--- python_one_liners/numpy_oneliners.py
+++ python_one_liners/numpy_oneliners.py
@@ -1,7 +1,16 @@
 import numpy as np
 
 
-def get_highest_after_tax_income(salaries, taxation):
+def get_person_with_highest_net_income(folks, salaries, taxation):
+    netIncomes = salaries * (1.0 - taxation)
+    maxNetIncome = np.max(netIncomes)
+    rowIndex = np.nonzero(netIncomes == maxNetIncome)[0][0]
+    return folks[rowIndex]
+    # netIncomes = salaries * (1.0 - taxation)
+    # folks[np.nonzero(netIncomes == np.max(netIncomes))[0][0]]
+
+
+def get_highest_net_income(salaries, taxation):
     afterTaxSalaries = salaries * (1.0 - taxation)
     return np.max(afterTaxSalaries)
     # one-liner would be np.max(salaries * (1.0 - taxation))
--- tests/test_numpy_one_liners.py
+++ tests/test_numpy_one_liners.py
@@ -1,6 +1,21 @@
 import numpy as np
 
-from python_one_liners import numpy_oneliners
+from python_one_liners import numpy_oneliners as npol
+
+
+def test_person_with_highest_after_tax_salary_should_be_Tim():
+    givenFolks = ["Alice", "Bob", "Tim"]
+    alice = [99, 101, 103]
+    bob = [110, 108, 105]
+    tim = [90, 88, 85]
+    givenSalaries = np.array([alice, bob, tim])
+    givenTaxation = np.array(
+        [[0.2, 0.25, 0.22], [0.4, 0.5, 0.5], [0.1, 0.2, 0.1]]
+    )
+    actual = npol.get_person_with_highest_net_income(
+        givenFolks, givenSalaries, givenTaxation
+    )
+    assert actual == "Tim"
 
 
 def test_highest_after_tax_salary_should_be_81():
@@ -11,7 +26,7 @@ def test_highest_after_tax_salary_should_be_81():
     givenTaxation = np.array(
         [[0.2, 0.25, 0.22], [0.4, 0.5, 0.5], [0.1, 0.2, 0.1]]
     )
-    actualHighestAfterTaxSalary = numpy_oneliners.get_highest_after_tax_income(
+    actualHighestNetIncome = npol.get_highest_net_income(
         givenSalaries, givenTaxation
     )
-    assert actualHighestAfterTaxSalary == 81
+    assert actualHighestNetIncome == 81
